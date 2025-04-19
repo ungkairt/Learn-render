f4f74bff51a577e269ac33385afd863db72edcf4
ungkairt <ungkairt7@gmail.com>
2025-04-19 18:08:24 +0700

    remove primary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_cable.mount_path = nil
-  config.action_cable.allowed_request_origins = []
-  config.action_cable.url = nil
 end
