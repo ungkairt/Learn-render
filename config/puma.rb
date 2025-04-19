f58246f266b7cff23eddd00bca17f5324fbe20f7
ungkairt <ungkairt7@gmail.com>
2025-04-19 18:21:02 +0700

    add cable

--- config/puma.rb
+++ config/puma.rb
@@ -36,6 +36,8 @@ plugin :tmp_restart
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
+workers ENV.fetch{"WEB_CONCURRENCY"} { 4 }
+
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
